23e3bb702fbeed0aecd9c57b67fa5ba8b6894e99
Lucile <developer.lvt@gmail.com>
2021-04-14 20:39:13 -0400

    added github pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <div>
       <Navbar />
       <Body />
-
+      <Carte />
       <Footer />
     </div>
   );
--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,11 +1,15 @@
 import React from "react";
 
+import img from "../assets/Img1.png"
+
 import "./Carte.css";
 
 function Carte() {
   return (
     <div className="carte-container" id="section2">
-     
+        <img src={img}></img>
+        <img src={img}></img>
+        
     </div>
   );
 }
