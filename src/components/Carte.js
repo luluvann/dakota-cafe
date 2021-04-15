e4c5b01bb037fffc34647840550c0a9e3423385a
Lucile <developer.lvt@gmail.com>
2021-04-14 21:49:36 -0400

    added new pics

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,14 +1,20 @@
 import React from "react";
 
-import img from "../assets/Img1.png"
+import img1 from "../assets/Img1.png";
+import img2 from "../assets/Img2.png"
+import img3 from "../assets/Img3.png"
+import img4 from "../assets/Img4.png"
+
 
 import "./Carte.css";
 
 function Carte() {
   return (
     <div className="carte-container" id="section2">
-        <img src={img}></img>
-        <img src={img}></img>
+        <img src={img1}></img>
+        <img src={img2}></img>
+        <img src={img3}></img>
+        <img src={img4}></img>
         
     </div>
   );
